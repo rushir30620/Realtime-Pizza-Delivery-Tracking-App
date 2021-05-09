7a8ca6680e49fe2d1c854eac52f20da2e04848e2
Rathod Rushikesh <rushir306@gmail.com>
2021-05-09 10:32:20 +0530

    add cart page.

--- server.js
+++ server.js
@@ -8,14 +8,30 @@ const PORT = process.env.PORT || 3000;
 //assets
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
 //set template engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+//cart route
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+});
+
+//login route
+app.get('/login', (req, res) => {
+    res.render('auth/login')
+});
+
+//register route
+app.get('/register', (req, res) => {
+    res.render('auth/register')
+});
 
 app.listen(PORT , () => {
     console.log(`Listening on port ${PORT}`);
