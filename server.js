104873cc9d608e8675cd7bc3c4023e00f9880fa9
Rathod Rushikesh <77226779+rushir30620@users.noreply.github.com>
2021-05-23 21:30:40 +0530

    change url

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(session({
     secret: process.env.COOKIE_SECRET,
     resave: false,
     store: MongoDbStore.create({
-        mongoUrl: 'mongodb://localhost:27017/pizza'
+        mongoUrl: process.env.MONGO_CONNECTION_URL
     }),
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 60 * 60 * 24} //24hours
